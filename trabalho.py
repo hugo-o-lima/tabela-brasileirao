ca1872ed53a174d0cc5ae8ef8d4dde1440cf366a
Hugo <hugo.antonio2026@gmail.com>
2025-07-16 18:27:42 -0300

    Refiz algumas partes do código

--- trabalho.py
+++ trabalho.py
@@ -15,10 +15,16 @@ def tira_quebra_de_linha(jogos: list[str]) -> list[str]:
 
 from dataclasses import dataclass
 @dataclass
-class nome_dos_times:
+class Jogo:
+    '''
+    Tipo de dado composto que representa as informacoes de um jogo
+    '''
     anfitriao: str
+    gols_anfitriao: int
     visitante: str
-def nome_das_equipes(jogo:str) -> nome_dos_times:
+    gols_visitante: int
+
+def nome_das_equipes(jogo:str) -> Jogo:
     """
     >>> nome_das_equipes("Fluminense 1 Cuiaba 0")
     nome_dos_times(anfitriao='Fluminense', visitante='Cuiaba')
@@ -31,14 +37,10 @@ def nome_das_equipes(jogo:str) -> nome_dos_times:
     anfitriao = jogo[:espacos[-3]]
     # O nome do visitante vai do espaço depois do placar do anfitrião até o espaço antes do placar do visitante
     visitante = jogo[espacos[-2] + 1:espacos[-1]]
-    jogos = nome_dos_times(anfitriao, visitante)
+    jogos = Jogo(anfitriao, 0 , visitante, 0)
     return jogos
 
-@dataclass
-class placar:
-    gols_anfitriao: int
-    gols_visitante: int
-def placar_do_jogo(jogo: str) -> placar:
+def placar_do_jogo(jogo: str) -> Jogo:
     """
     >>> placar_do_jogo("Fluminense 1 Cuiaba 0")
     placar(gols_anfitriao=1, gols_visitante=0)
@@ -51,9 +53,9 @@ def placar_do_jogo(jogo: str) -> placar:
     gols_anfitriao = int(jogo[espacos[-3] + 1:espacos[-2]])
     # O placar do visitante está após o último espaço
     gols_visitante = int(jogo[espacos[-1] + 1:])
-    return placar(gols_anfitriao, gols_visitante)
+    return Jogo('', gols_anfitriao,'', gols_visitante)
 
-def vitorias(placar: placar) -> int:
+def vitorias(placar: Jogo) -> int:
     """
     >>> vitorias(placar(gols_anfitriao=1, gols_visitante=0))
     1
@@ -61,15 +63,13 @@ def vitorias(placar: placar) -> int:
     0
     """
     vitorias = 0
-    if placar.gols_anfitriao > placar.gols_visitante:
+    if Jogo.gols_anfitriao > Jogo.gols_visitante:
         vitorias += 1
-    elif placar.gols_anfitriao < placar.gols_visitante:
-        vitorias += 0
-    else:
+    if Jogo.gols_anfitriao < Jogo.gols_visitante:
         vitorias += 0
     return vitorias
 
-def saldo_de_gols(placar: placar) -> int:
+def saldo_de_gols(placar: Jogo) -> int:
     """
     >>> saldo_de_gols(placar(gols_anfitriao=1, gols_visitante=0))
     1
@@ -77,28 +77,25 @@ def saldo_de_gols(placar: placar) -> int:
     0
     """
     saldo = 0
-    saldo = placar.gols_anfitriao - placar.gols_visitante
+    saldo = Jogo.gols_anfitriao - Jogo.gols_visitante
     return saldo
-@dataclass
-class Pontos:
-    pontos: int
-def ganho_pontos(placar: placar) -> Pontos:
+
+def ganho_pontos(x: Jogo) -> int:
     """
-    >>> ganho_pontos(placar(gols_anfitriao=1, gols_visitante=0))
-    Pontos(pontos=3)
-    >>> ganho_pontos(placar(gols_anfitriao=0, gols_visitante=0))
-    Pontos(pontos=1)
-    >>> ganho_pontos(placar(gols_anfitriao=0, gols_visitante=1))
-    Pontos(pontos=3)
+    >>> ganho_pontos(placar(1, 0), anfitriao=True)
+    3
+    >>> ganho_pontos(placar(0, 1), anfitriao=False)
+    3
+    >>> ganho_pontos(placar(1, 1), anfitriao=True)
+    1
     """
-    pontos = 0
-    if placar.gols_anfitriao > placar.gols_visitante:
-        pontos += 3
-    elif placar.gols_visitante > placar.gols_anfitriao:
-        pontos += 3
+    if x.gols_anfitriao > x.gols_visitante:
+        pontos = 3
+    elif x.gols_visitante > x.gols_anfitriao:
+        pontos = 3
     else:
-        pontos += 1
-    return Pontos(pontos)
+        pontos = 1
+    return pontos
 
 @dataclass
 class estatisticas:
@@ -117,40 +114,103 @@ def verificar_elemento(lista, elemento_procurado) -> bool:
             encontrado = True
     return encontrado
 
-def valor_letra(letra: str) -> int:
-    '''
-    Retorna o numero que indica a posição de uma letra no alfabeto
-    '''
-    x = 0
-    minusculas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-                  "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-                  "u", "v", "w", "x", "y", "z"]
-    maiusculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-                  "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-                  "U", "V", "W", "X", "Y", "Z"]
-    for i in range(len(minusculas)):
-        if letra == minusculas[i] or letra == maiusculas[i]:
-            x = i
+def deve_trocar(a: estatisticas, b: estatisticas) -> bool:
+    """
+    Retorna True se o time b deve vir antes do time a, de acordo com os critérios:
+    - Pontos (maior primeiro)
+    - Vitórias (maior primeiro)
+    - Saldo de gols (maior primeiro)
+    - Ordem alfabética (nome menor vem antes)
+
+    >>> a = estatisticas("Flamengo", 3, 1, 1)
+    >>> b = estatisticas("Botafogo", 4, 1, 1)
+    >>> deve_trocar(a, b)
+    True
+
+    >>> a = estatisticas("Flamengo", 4, 1, 1)
+    >>> b = estatisticas("Botafogo", 4, 1, 1)
+    >>> deve_trocar(a, b)
+    True
+
+    >>> a = estatisticas("Botafogo", 4, 1, 1)
+    >>> b = estatisticas("Flamengo", 4, 1, 1)
+    >>> deve_trocar(a, b)
+    False
+    """
+    x = False
+    if b.pontos > a.pontos:
+        x = True
+    if b.pontos == a.pontos:
+        if b.vitorias > a.vitorias:
+            x = True
+        if b.vitorias == a.vitorias:
+            if b.saldo_de_gols > a.saldo_de_gols:
+                x =True
+            if b.saldo_de_gols == a.saldo_de_gols:
+                if b.nome < a.nome:
+                    x = True
     return x
+
+
+
 def ordena_times(estat: list[estatisticas]):
+    """
+    Ordena a lista de estatísticas dos times com base nos critérios definidos:
+    pontos, vitórias, saldo de gols e nome.
+    """
     for i in range(len(estat)):
         for j in range(i + 1, len(estat)):
-            troca = False
-
-            if estat[j].pontos > estat[i].pontos:
-                troca = True
-            elif estat[j].pontos == estat[i].pontos:
-                if estat[j].vitorias > estat[i].vitorias:
-                    troca = True
-                elif estat[j].vitorias == estat[i].vitorias:
-                    if estat[j].saldo_de_gols > estat[i].saldo_de_gols:
-                        troca = True
-                    elif estat[j].saldo_de_gols == estat[i].saldo_de_gols:
-                        # Usando valor da primeira letra com sua função valor_letra
-                        if valor_letra(estat[j].nome[0]) < valor_letra(estat[i].nome[0]):
-                            troca = True
-            if troca:
+            if deve_trocar(estat[i], estat[j]):
                 estat[i], estat[j] = estat[j], estat[i]
+
+def list_str_para_jogo(lista: list[str]) -> list[Jogo]:
+    """
+    Converte lista de strings para lista de objetos Jogo, usando funções nome_das_equipes e placar_do_jogo.
+
+    Exemplo:
+    >>> jogos_str = ["Flamengo 2 Vasco 1", "Botafogo 3 Flamengo 2"]
+    >>> list_str_para_jogo(jogos_str)
+    [Jogo(anfitriao='Flamengo', gols_anfitriao=2, visitante='Vasco', gols_visitante=1),
+     Jogo(anfitriao='Botafogo', gols_anfitriao=3, visitante='Flamengo', gols_visitante=2)]
+    """
+    jogos = []
+    for linha in lista:
+        nomes = nome_das_equipes(linha)
+        placar_atual = placar_do_jogo(linha)
+        jogos.append(Jogo(
+            anfitriao=nomes.anfitriao,
+            gols_anfitriao=placar_atual.gols_anfitriao,
+            visitante=nomes.visitante,
+            gols_visitante=placar_atual.gols_visitante
+        ))
+    return jogos
+
+def atualiza_estatisticas(nome_time: str, gols_feitos: int, gols_sofridos: int,nomes: list[str], estat: list[estatisticas]):
+    """
+    Atualiza ou cria as estatísticas do time com base no resultado do jogo.
+
+    - Se o time ainda não estiver cadastrado, ele será adicionado às listas.
+    - Os pontos, vitórias e saldo de gols são atualizados com base nos gols feitos e sofridos.
+
+    Exemplo:
+    >>> nomes = []
+    >>> estat = []
+    >>> atualiza_estatisticas("Flamengo", 2, 1, nomes, estat)
+    >>> estat[0]
+    estatisticas(nome='Flamengo', pontos=3, vitorias=1, saldo_de_gols=1)
+    """
+    if not verificar_elemento(nomes, nome_time):
+        nomes.append(nome_time)
+        estat.append(estatisticas(nome_time, 0, 0, 0))
+
+    for i in range(len(estat)):
+        if estat[i].nome == nome_time:
+            if gols_feitos > gols_sofridos:
+                estat[i].pontos += 3
+                estat[i].vitorias += 1
+            elif gols_feitos == gols_sofridos:
+                estat[i].pontos += 1
+            estat[i].saldo_de_gols += (gols_feitos - gols_sofridos)
 
 def classifica_times(jogos: list[str]) -> list[estatisticas]:
     nomes: list[str]= []
@@ -160,39 +220,16 @@ def classifica_times(jogos: list[str]) -> list[estatisticas]:
         nomes_do_jogo = nome_das_equipes(jogos[i])
         placar_do_jogo_atual = placar_do_jogo(jogos[i])
 
-        # ----------- ANFITRIÃO -----------
-        if not verificar_elemento(nomes, nomes_do_jogo.anfitriao):
-            nomes.append(nomes_do_jogo.anfitriao)
-            nova_estat = estatisticas(nomes_do_jogo.anfitriao, 0, 0, 0)
-            estat.append(nova_estat)
+        # Atualiza anfitrião
+        atualiza_estatisticas(
+            nomes_do_jogo.anfitriao,placar_do_jogo_atual.gols_anfitriao,
+            placar_do_jogo_atual.gols_visitante, nomes, estat
+        )
+        # Atualiza visitante
+        atualiza_estatisticas(
+            nomes_do_jogo.visitante, placar_do_jogo_atual.gols_visitante,
+            placar_do_jogo_atual.gols_anfitriao, nomes, estat)
 
-        for j in range(len(nomes)):
-            if nomes[j] == nomes_do_jogo.anfitriao:
-                if placar_do_jogo_atual.gols_anfitriao > placar_do_jogo_atual.gols_visitante:
-                    estat[j].pontos = estat[j].pontos + 3
-                    estat[j].vitorias = estat[j].vitorias + 1
-                elif placar_do_jogo_atual.gols_anfitriao == placar_do_jogo_atual.gols_visitante:
-                    estat[j].pontos = estat[j].pontos + 1
-                # saldo de gols do anfitrião
-                saldo = placar_do_jogo_atual.gols_anfitriao - placar_do_jogo_atual.gols_visitante
-                estat[j].saldo_de_gols = estat[j].saldo_de_gols + saldo
-
-        # ----------- VISITANTE -----------
-        if not verificar_elemento(nomes, nomes_do_jogo.visitante):
-            nomes.append(nomes_do_jogo.visitante)
-            nova_estat = estatisticas(nomes_do_jogo.visitante, 0, 0, 0)
-            estat.append(nova_estat)
-
-        for j in range(len(nomes)):
-            if nomes[j] == nomes_do_jogo.visitante:
-                if placar_do_jogo_atual.gols_visitante > placar_do_jogo_atual.gols_anfitriao:
-                    estat[j].pontos = estat[j].pontos + 3
-                    estat[j].vitorias = estat[j].vitorias + 1
-                elif placar_do_jogo_atual.gols_visitante == placar_do_jogo_atual.gols_anfitriao:
-                    estat[j].pontos = estat[j].pontos + 1
-                # saldo de gols do visitante
-                saldo = placar_do_jogo_atual.gols_visitante - placar_do_jogo_atual.gols_anfitriao
-                estat[j].saldo_de_gols = estat[j].saldo_de_gols + saldo
     ordena_times(estat)
     return estat
 
